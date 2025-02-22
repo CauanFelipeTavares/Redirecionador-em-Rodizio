1b8dc66db6cd4e53d68e6162b76767b5c5330090
CauanFelipeTavares <tavarescauanf@gmail.com>
2025-02-22 10:11:34 -0300

    feat: new contact

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 8000
-const links = [
+const links = [ 
     'https://api.whatsapp.com/send/?phone=5511932523135&text&type=phone_number&app_absent=0',
     'https://api.whatsapp.com/send/?phone=5511970502899&text&type=phone_number&app_absent=0',
+    'https://api.whatsapp.com/send/?phone=5511937714586&text&type=phone_number&app_absent=0',
 ]
 let index = 0
 
