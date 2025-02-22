806db33ef05e987c84725a2645a66ed04fa948c6
CauanFelipeTavares <tavarescauanf@gmail.com>
2025-02-22 10:10:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 8000
+const links = [
+    'https://api.whatsapp.com/send/?phone=5511932523135&text&type=phone_number&app_absent=0',
+    'https://api.whatsapp.com/send/?phone=5511970502899&text&type=phone_number&app_absent=0',
+]
+let index = 0
+
+app.get('/', (req, res) => {
+    res.redirect(links[index++ % links.length])
+})
+
+app.listen(PORT)
